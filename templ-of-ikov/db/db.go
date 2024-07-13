e477593f8f11afcf38b934015126acee5177ae4c
Calvin Lapp <lapp.cd@gmail.com>
2024-07-12 22:43:30 -0400

    DB

---
+++ templ-of-ikov/db/db.go
@@ -0,0 +1,11 @@
+package db
+
+import "github.com/jmoiron/sqlx"
+
+func NewDB(dataSourceName string) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
